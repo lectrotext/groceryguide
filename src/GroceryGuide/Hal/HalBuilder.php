1a2603e4088456a756dba8f09e723f0948d4014a
James Pozenel <lectrotext@gmail.com>
2015-01-19 14:16:07 -0500

    added psr-4 lookup. DI Provider fleshed out a bit

---
+++ src/GroceryGuide/Hal/HalBuilder.php
@@ -0,0 +1,19 @@
+<?php
+
+use \Nocarrier\Hal;
+
+class HalBuilder
+{
+    private $resource;
+
+    private $links;
+
+    private $embed;
+
+    public static function __construct (array $resource, array $links, array $embed = [])
+    {
+        $this->resource = $resource;
+        $this->links = $links;
+        $this->embed = $embed;
+    }
+}
