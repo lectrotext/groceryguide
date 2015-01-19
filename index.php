41f104a58ab11412042b2866f7caf6701088648c
James Pozenel <lectrotext@gmail.com>
2015-01-19 02:10:04 -0500

    markets and produce stood up, limit 100

--- index.php
+++ index.php
@@ -37,9 +37,10 @@ $app->group('/api', function() use ($app) {
 	$app->get('/', function() use ($app) {
         $links = ["_links" => [
             'self'      => $app->request->getURL() . $app->request->getPath(),
-            'rfp:items'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/items/{id}', "templated"=> true],
-            'rfp:produce'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/produce/{id}', "templated"=> true],
-            'rfp:stores'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/stores/{id}', "templated"=> true]
+            'rfp:items'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/items'],
+            'rfp:produce'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/produce'],
+            'rfp:stores'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/stores'],
+            'rfp:markets'   =>  ["href" => $app->request->getURL() . $app->request->getPath() . '/markets']
         ]];
 
 //        $hal = new Hal($app->request->getURL() . $app->request->getPath());
@@ -58,16 +59,72 @@ $app->group('/api', function() use ($app) {
                 var_dump($row);
             }
         } elseif ($app->request->isGet() && $id == null) {
-            $result = $app->dep{'db'}->query("SELECT * FROM produce");
+            $result = $app->dep{'db'}->query("SELECT * FROM produce LIMIT 100");
+
             while ($row = $result->fetch()) {
-                var_dump($row);
+                $collection['produce'][] = $row;
             }
+
+            $links = ["_links" => [
+                'self'      =>  ["href" => $app->request->getURL() . $app->request->getPath()],
+                'index'     =>  ["href" => dirname($app->request->getURL() . $app->request->getPath())],
+                'search'    =>  ["href" =>  $app->request->getURL() . $app->request->getPath() . "/{id}", "templated" => true]
+            ]];
+
+            $resource = array_merge($collection, $links);
+
+            $hal = Hal::fromJson(json_encode($resource));
+	        echo $hal->asJson();
         }
     });
 
     $app->get('/stores(/:id)', function($id = null) use ($app) {
         if ($app->request->isGet() && $id != null) {
             $result = $app->dep{'db'}->query("SELECT * FROM stores WHERE id = $id");
+
+            $row = $result->fetch();
+
+            $resource = '';
+            if (!empty($row)) {
+              $resource = $row;
+            }
+
+            $links = ["_links" => [
+                'self'      => ["href" => $app->request->getURL() . $app->request->getPath()],
+                'index'   =>  ["href" => dirname($app->request->getURL() . $app->request->getPath())],
+                'search'    =>  ["href" =>  $app->request->getURL() . $app->request->getPath() . "/{id}", "templated" => true]
+            ]];
+            
+            $resource = array_merge($resource, $links);
+
+            $hal = Hal::fromJson(json_encode($resource));
+	        echo $hal->asJson();
+        
+            
+        } elseif ($app->request->isGet() && $id == null) {
+            $result = $app->dep{'db'}->query("SELECT * FROM stores");
+
+            $collection = null;
+            while ($row = $result->fetch()) {
+                $collection['stores'][] = $row;
+            }
+
+            $links = ["_links" => [
+                'self'      =>  ["href" => $app->request->getURL() . $app->request->getPath()],
+                'index'     =>  ["href" => dirname($app->request->getURL() . $app->request->getPath())],
+                'search'    =>  ["href" =>  $app->request->getURL() . $app->request->getPath() . "/{id}", "templated" => true]
+            ]];
+            
+            $resource = array_merge($collection, $links);
+
+            $hal = Hal::fromJson(json_encode($resource));
+	        echo $hal->asJson();
+        }
+    });
+
+    $app->get('/markets(/:id)', function($id = null) use ($app) {
+        if ($app->request->isGet() && $id != null) {
+            $result = $app->dep{'db'}->query("SELECT * FROM markets WHERE id = $id");
 
             $row = $result->fetch();
 
@@ -88,11 +145,11 @@ $app->group('/api', function() use ($app) {
         
             
         } elseif ($app->request->isGet() && $id == null) {
-            $result = $app->dep{'db'}->query("SELECT * FROM stores");
+            $result = $app->dep{'db'}->query("SELECT * FROM markets LIMIT 100");
 
             $collection = null;
             while ($row = $result->fetch()) {
-                $collection[] = $row;
+                $collection['markets'][] = $row;
             }
 
             $links = ["_links" => [
